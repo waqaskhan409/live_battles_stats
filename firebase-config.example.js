92d1bab7e30a650ba9f2baf9f881ddf82c928f92
Waqas Khan <waqaskhan@Waqass-MacBook-Pro-2.local>
2026-03-19 14:44:22 +0500

    Added date dialogue and bulk deletion

---
+++ firebase-config.example.js
@@ -0,0 +1,14 @@
+// Copy this file to firebase-config.js and fill in your values.
+// firebase-config.js is gitignored and must NOT be committed.
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWJ5P0lEA93WDPg2jw4QCw8H_MO1zx950",
+  authDomain: "bwcnnct.firebaseapp.com",
+  databaseURL: "https://bwcnnct.firebaseio.com",
+  projectId: "bwcnnct",
+  storageBucket: "bwcnnct.appspot.com",
+  messagingSenderId: "859114797531",
+  appId: "1:859114797531:web:e542b4efc659f0e01b0ffe",
+  measurementId: "G-L3DKR7CNJN"
+};
+
+firebase.initializeApp(firebaseConfig);
